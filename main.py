86b2d58e011afb9c13daabe4dcb06ec3c484b3e0
AdamOlszewski <65673@student.ue.poznan.pl>
2022-05-29 22:39:31 +0200

    first

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import csv
+calls_sum_dict = dict()
+
+with open('phoneCalls.csv', 'r') as fin:
+    # csv.DictReader uses first line in file for column headings by default
+    reader = csv.reader(fin, delimiter = ";") # comma is default delimiter
+    headers = next(reader)
+    for row in reader:
+      from_subscr = int(row[0])
+      if from_subscr not in calls_sum_dict:
+        calls_sum_dict[from_subscr] = 0
+      # value = int(row[3])
+      value =1
+      calls_sum_dict[from_subscr] += value
 
 if __name__ == "__main__":
     pass
