ad8fab116a58fdbb7732ea35187fb820b67bbd8e
AdamOlszewski <65673@student.ue.poznan.pl>
2022-05-29 23:00:27 +0200

    3.0

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import csv
-calls_sum_dict = dict()
-def Polaczenia(self):
-    with open('phoneCalls.csv', 'r') as fin:
-        # csv.DictReader uses first line in file for column headings by default
-        reader = csv.reader(fin, delimiter=",")  # comma is default delimiter
-        headers = next(reader)
-        for row in reader:
-            from_subscr = int(row[0])
-            if from_subscr not in calls_sum_dict:
-                calls_sum_dict[from_subscr] = 0
-            # value = int(row[3])
-            value = 1
-            calls_sum_dict[from_subscr] += value
 
-def pobierz_najczesciej_dzwoniacego(self):
-    return max(self.calls_sum_dict.items(), key= lambda x: x[1])
+class MenadzerPolaczen:
+  def __init__(self, filename):
+    self.filenaCme = filename
+    self.data_dict = self.read_data()
+
+  def read_data(self):
+    calls_dict_sum = dict()
+    with open(self.filename, 'r') as fin:
+      reader = csv.reader(fin, delimiter= ",")
+      headers = next(reader)
+
+      for row in reader:
+        from_subsr = int(row[0])
+        if from_subsr not in calls_dict_sum:
+          calls_dict_sum[from_subsr] = 0
+        calls_dict_sum[from_subsr] += 1
+    return calls_dict_sum
+
+  def pobierz_najczesciej_dzwoniacego(self):
+    return max(self.data_dict.items(), key= lambda x: x[1])
 
 if __name__ == '__main__':
-    print(Polaczenia(input()).pobierz_najczesciej_dzwoniacego())
+    print(MenadzerPolaczen(input()).pobierz_najczesciej_dzwoniacego())
